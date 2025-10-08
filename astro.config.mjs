a28b48428fb55b7598e243c0cef83d067260687c
Miabread <miabreadloaf@gmail.com>
2025-10-08 11:39:04 -0400

    Add bundle visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://miabread.github.io/',
     vite: {
-        plugins: [tailwindcss()],
+        plugins: [
+            tailwindcss(),
+            visualizer({
+                emitFile: true,
+                filename: 'stats.html',
+            }),
+        ],
     },
     integrations: [icon()],
 });
