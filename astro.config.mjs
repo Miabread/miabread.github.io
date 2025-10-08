4dcebf52bac3dc98a6f48b74bd6770cbc4f3eacc
Miabread <miabreadloaf@gmail.com>
2025-10-08 13:43:07 -0400

    Change icon framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon';
 import { visualizer } from 'rollup-plugin-visualizer';
+import Icons from 'unplugin-icons/vite';
+
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://miabread.github.io/',
+
     vite: {
         plugins: [
             tailwindcss(),
@@ -14,7 +17,11 @@ export default defineConfig({
                 emitFile: true,
                 filename: 'stats.html',
             }),
+            Icons({
+                compiler: 'solid',
+                autoInstall: true,
+            }),
         ],
     },
-    integrations: [icon()],
+    integrations: [solidJs()],
 });
